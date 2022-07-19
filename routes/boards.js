ceeeb305fa8f4cf35a9e65bfca713ef8214c3379
dana <dana@example.com>
2022-07-19 17:55:33 +0900

    게시판 수정 페이지 이동완료 TODO : 게시판의 내용을 수정하는 함수와 게시판의 데이터를 삭제하는 함수 구현해야함

--- routes/boards.js
+++ routes/boards.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var mysql = require('mysql'); //mysql 모듈을 로딩.
 const ejs = require('ejs');
 
+
+// TODO : 게시판의 내용을 수정하는 함수와 게시판의 데이터를 삭제하는 함수 구현해야함
+
+
 // DB settings
 var mysql = require('mysql');
 var connection = mysql.createConnection({
@@ -14,6 +18,8 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+// 게시판 첫 화면에서 DB값 띄우기
+// Boards/index
 router.get('/', function (req, res, next) {
     connection.query('select bd_no,bd_title,bd_hit,DATE_FORMAT(bd_date, "%Y/%m/%d %T") as bd_date, bt_text from boards order by bd_no desc', function (err, rows) {
         if (err) console.log(err);        // 만약 에러값이 존재한다면 로그에 표시합니다.
@@ -25,6 +31,8 @@ router.get('/', function (req, res, next) {
     });
 });
 
+// 게시판에서 제목을 클릭해서 상세 내역을 볼 수 있게 하는 함수
+// Boards/show
 router.get('/show/:bd_no', function (req, res) {
     var bd_no = req.params.bd_no;
     console.log("bd_no : " + bd_no);
@@ -57,10 +65,13 @@ router.get('/show/:bd_no', function (req, res) {
     })
 })
 
+// GET 게시판에서 새로 내용을 작성하는 함수
 router.get('/new', function (req, res){
     res.render('boards/new');
 })
 
+// POST 게시판에서 새로 내용을 작성하는 함수
+// POST는 form과 항상 같이 이동
 router.post('/new', function (req, res) {
     // 제목과 내용 변수값 저장
     var bd_no = req.body.bd_no
@@ -98,13 +109,49 @@ router.post('/new', function (req, res) {
             })
         })
     })
-
-
 })
 
+// GET 게시판의 내용을 수정하기 위한 페이지로 이동
+router.get('/show/:bd_no/edit', function (req, res){
+    var bd_no = req.params.bd_no;
 
+    connection.beginTransaction(function (err) {
+        if (err) console.log(err);
+    connection.query('select bd_no,bd_writer,bd_title,bd_hit,DATE_FORMAT(bd_date, "%Y/%m/%d %T") as moddate, DATE_FORMAT(bd_date, "%Y/%m/%d %T") as bd_date, bt_text from boards where bd_no=?', [bd_no], function (err, rows) {
+        if (err) {
+            console.log(err);
+            connection.rollback(function () {
+                console.error('rollback error!!');
+            })
+        }
+        else {
+            connection.commit(function (err) {
+                if (err) console.log(err);
+                console.log('show row : ' + rows);
+                res.render('boards/edit', { title: rows[1], rows: rows });
+            })
+        }
+    })
+})
+})
 
+// POST 게시판의 내용을 수정하기 위한 함수
+// 현재 구현하는 중
+router.post('/update', function(req, res){
+    // 제목과 내용 변수값 저장
+    var title = req.body.title
+    var bd_text = req.body.bd_text
+    var bd_writer = req.body.bd_writer
+    
+    connection.beginTransaction(function(err){
+        if(err) console.log(err);
 
-
+    connection.query('update boards set bd_title=? , bt_text=? where bd_writer=?',[title, bd_text, bd_writer], function(err ,result){
+        if(err) throw err;
+        console.log('수정한 결과값 = ', result);
+        res.redirect('/boards');
+    })
+})
+})
 
 module.exports = router;
