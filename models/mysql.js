c8a6496b3f04ff8bd3e408a139c2dbe1bac38e2d
dana <pddana@naver.com>
2022-10-17 17:05:29 +0900

    mysql-model commit

--- models/mysql.js
+++ models/mysql.js
@@ -1,10 +1,18 @@
 // DB settings
 const mysql = require('mysql');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path:path.join(__dirname, './.env')
+});
+
+
 const connection = mysql.createConnection({
-    host: '{DBHostAddress}',
-    user: '{DBUser}',
-    password: '{DBPassword}',
-    database: '{DBName}'
+    host: process.env.DBHostAddress ,
+    user: process.env.DBUser,
+    password: process.env.DBPassword,
+    database: process.env.DBName
 });
 
 module.exports = connection;
