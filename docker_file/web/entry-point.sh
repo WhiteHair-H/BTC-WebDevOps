cab32469ac01da215ce3ef43b3ae5fea1bb73375
dana <pddana@naver.com>
2022-10-18 17:23:48 +0900

    10/18 web-svc commit

--- docker_file/web/entry-point.sh
+++ docker_file/web/entry-point.sh
@@ -3,10 +3,10 @@ cat >>conf/httpd.conf<<EOF
 LoadModule proxy_module modules/mod_proxy.so
 LoadModule proxy_http_module modules/mod_proxy_http.so
 <VirtualHost *:80>
-  ServerName 20.194.109.211
+  ServerName $ServerName
   ProxyPreserveHost On
-  ProxyPass / http://172.17.0.3:8081/
-  ProxyPassReverse / http://172.17.0.3:8081/
+  ProxyPass / http://$WasServer:8081/
+  ProxyPassReverse / http://$WasServer:8081/
 </VirtualHost>
 EOF
 /usr/local/apache2/bin/apachectl -D FOREGROUND
